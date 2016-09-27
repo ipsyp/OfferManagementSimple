e95660ffbf6021d46d1b5e8039d10856f7318bc2
Ioannis Giakoumis <mrp_gi@hotmail.com>
2016-09-27 15:42:03 +0300

    changes

--- OfferManagement/OfferManagement.Portal/Properties/AssemblyInfo.cs
+++ OfferManagement/OfferManagement.Portal/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Web.UI;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ArmyLaptops")]
+[assembly: AssemblyTitle("OfferManagement")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("None")]
-[assembly: AssemblyProduct("ArmyLaptops")]
+[assembly: AssemblyProduct("OfferManagement")]
 [assembly: AssemblyCopyright("Copyright © None 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
